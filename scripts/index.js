60a1de0c37874785b53d9b950e663652595ea190
Kirill <sergeew_k@mail.ru>
2022-02-15 19:15:14 +0300

    oh...this modules....hate them

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,30 @@
+import Person from './classes'
+
 const form = document.querySelector('.form-input')
-const inputs = form.querySelectorAll('.form-input__item')
+const inputs = form.querySelectorAll('.form-input')
+const btnSave = document.getElementById('btn-save')
+const btnDownload = document.getElementById('btn-download')
+
+const firstTester = new Person({
+  name: 'Pavel',
+  age: 19,
+  message: 'Darov!',
+})
+const dataBase = [firstTester]
 
 form.addEventListener('submit', (e) => {
   e.preventDefault()
-  console.log('hello')
 })
 
-for (let i = 0; i < inputs.length; i++) {
-  console.log(inputs[i])
-}
+btnSave.addEventListener('click', (e) => {
+  console.log('send')
 
-console.log(form)
+  const tester = new Person()
+
+  for (let i = 0; i < inputs.length; i++) {
+    console.log(tester.name)
+  }
+  dataBase.push(tester)
+  console.log(dataBase)
+})
+console.log(dataBase)
