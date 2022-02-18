2a9b4555339020fa1b3252b277e23b0e5459e85c
Kirill <sergeew_k@mail.ru>
2022-02-18 17:51:42 +0300

    fix number in array, continue to create view array

--- scripts/index.js
+++ scripts/index.js
@@ -1,81 +1,73 @@
-const form = document.querySelector('.form-input')
-const inputs = form.querySelectorAll('.form-input__input')
-const btnSave = document.getElementById('btn-save')
-const btnDownload = document.getElementById('btn-download')
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector('.form-input')
+  const inputs = form.querySelectorAll('.form-input__input')
+  const btnSave = document.getElementById('btn-save')
+  const btnDownload = document.getElementById('btn-download')
 
-class Person {
-  constructor(options) {
-    this.name = options.name
-    this.age = options.age
-    this.message = options.message
-  }
-}
-
-const firstTester = new Person({
-  name: 'Pavel',
-  age: 19,
-  message: 'Darov!',
-})
-const secondTester = new Person({
-  name: 'Nikita',
-  age: 17,
-  message: 'Grettings!',
-})
-const dataBase = [firstTester, secondTester]
-
-form.addEventListener('submit', (e) => {
-  e.preventDefault()
-})
-
-btnSave.addEventListener('click', () => {
-  const tempTester = new Person({})
-  let inputHaveSomething = false
-
-  for (let i = 0; i < inputs.length; i++) {
-    if (inputs[i].value === '') {
-      inputHaveSomething = false
-      alert('Пустые инпуты!')
-      break
+  class Person {
+    constructor(options) {
+      this.name = options.name
+      this.age = options.age
+      this.message = options.message
     }
-    inputHaveSomething = true
   }
-  if (inputHaveSomething) {
-    for (let k = 0; k < inputs.length; k++) {
-      if (k === 0) {
-        tempTester.name = inputs[k].value
-        inputs[k].value = ''
+
+  const firstTester = new Person({
+    name: 'Pavel',
+    age: 19,
+    message: 'Darov!',
+  })
+  const secondTester = new Person({
+    name: 'Nikita',
+    age: 17,
+    message: 'Grettings!',
+  })
+  const dataBase = [firstTester, secondTester]
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+  })
+
+  btnSave.addEventListener('click', () => {
+    const tempTester = new Person({})
+    let inputHaveSomething = false
+
+    for (let i = 0; i < inputs.length; i++) {
+      if (inputs[i].value === '') {
+        inputHaveSomething = false
+        alert('Пустые инпуты!')
+        break
       }
-      if (k === 1) {
-        tempTester.age = inputs[k].value
-        inputs[k].value = ''
-      }
-      if (k === 2) {
-        tempTester.message = inputs[k].value
-        inputs[k].value = ''
-      }
+      inputHaveSomething = true
     }
-    dataBase.push(tempTester)
-    console.log('send')
-  }
+    if (inputHaveSomething) {
+      for (let k = 0; k < inputs.length; k++) {
+        if (k === 0) {
+          tempTester.name = inputs[k].value
+          inputs[k].value = ''
+        }
+        if (k === 1) {
+          tempTester.age = inputs[k].value
+          inputs[k].value = ''
+        }
+        if (k === 2) {
+          tempTester.message = inputs[k].value
+          inputs[k].value = ''
+        }
+      }
+      dataBase.push(tempTester)
+      console.log('send')
+    }
 
-  console.log(dataBase)
+    console.log(dataBase)
+  })
+
+  const list = document.querySelectorAll('.grid-big__list')
+
+  btnDownload.addEventListener('click', () => {
+    for (i = 0; i < inputs.length; i++) {
+      list[i].insertAdjacentHTML('beforeend', `<li>Hello</span>`)
+    }
+    console.log(list)
+  })
 })
-
-const gridSmallTable = document.querySelector('.grid-big')
-btnDownload.addEventListener('click', () => {
-  const newBLock = document.createElement('div')
-  newBLock.classList.add('.grid-small-item')
-  newBLock.insertAdjacentHTML('afterbegin', `<span>Hello</span>`)
-  // for (let i = 0; i < gridSmallTable.length; i++) {
-  //   // console.log(gridSmallTable[i])
-  //   const newELement = document.createElement('div')
-  //   newELement.insertAdjacentHTML(
-  //     'afterbegin',
-  //     `<span class="gridSmallItem" >Hello</span>`
-  //   )
-  //   // newELement.classList.add('.gridSmallItem')
-  // }
-  // console.log(dataBase)
-})
-
-console.log(gridSmallTable)
